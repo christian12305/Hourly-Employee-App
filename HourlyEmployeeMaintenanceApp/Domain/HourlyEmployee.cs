eb0a2900257ae2944c9248d0c876ee398e2ea629
Christian <ramoschristian39@gmail.com>
2023-04-17 23:47:14 -0400

    Updated the date of the file

--- HourlyEmployeeMaintenanceApp/Domain/HourlyEmployee.cs
+++ HourlyEmployeeMaintenanceApp/Domain/HourlyEmployee.cs
@@ -2,7 +2,7 @@
  * File: HourlyEmployee.cs
  * Author: Christian Ramos Ortega 841184582
  * Course: COTI 4150-KJ1 Prof. Antonio F. Huertas
- * Date: 04/15/2023
+ * Date: 04/17/2023
  * Purpose: This class is for the hourly employee.
  */
 namespace HourlyEmployeeMaintenanceApp.Domain
