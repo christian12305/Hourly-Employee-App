5365f840d89bb04576e8076d854f410ed0e0da1b
Christian <ramoschristian39@gmail.com>
2023-04-18 22:46:09 -0400

    Worked on constructor and a few methods for the Hourly Employee List. Added a description to the EmployeeDA class.

--- HourlyEmployeeMaintenanceApp/DataAccess/EmployeeDA.cs
+++ HourlyEmployeeMaintenanceApp/DataAccess/EmployeeDA.cs
@@ -2,15 +2,17 @@
  * File: EmployeeDA.cs
  * Author: Christian Ramos Ortega 841184582
  * Course: COTI 4150-KJ1 Prof. Antonio F. Huertas
- * Date: 04/15/2023
+ * Date: 04/18/2023
  * Purpose: This class contains access to the data of employees.
  */
+using HourlyEmployeeMaintenanceApp.Domain;
+
 namespace HourlyEmployeeMaintenanceApp.DataAccess
 {
     /// <summary>
-    /// 
+    /// This class represents an employee data access layer.
     /// </summary>
-    internal class EmployeeDA
+    public static class EmployeeDA
     {
     }
 }
--- HourlyEmployeeMaintenanceApp/Domain/HourlyEmployeeList.cs
+++ HourlyEmployeeMaintenanceApp/Domain/HourlyEmployeeList.cs
@@ -5,12 +5,14 @@
  * Date: 04/15/2023
  * Purpose: This class contains the list for the hourly employee.
  */
+using HourlyEmployeeMaintenanceApp.DataAccess;
+
 namespace HourlyEmployeeMaintenanceApp.Domain
 {
     public class HourlyEmployeeList
     {
         /// <summary>The contents of the internal list of hourly employees. </summary>
-        private List<HourlyEmployee> hourlyEmployees;
+        private List<HourlyEmployee> contents;
 
         /// <summary>
         /// Delegate for the change event handler.
@@ -18,5 +20,25 @@ namespace HourlyEmployeeMaintenanceApp.Domain
         /// <param name="employees">The list of hourly employees</param>
         public delegate void ChangeEventHandler(HourlyEmployeeList employees);
 
+        /// <summary>
+        /// Event that signals that the contents of the list of products has changed.
+        /// </summary>
+        public event ChangeEventHandler? ContentsChanged;
+
+        /// <summary>
+        /// Creates a new list of products.
+        /// </summary>
+        public HourlyEmployeeList()
+        {
+            contents = new List<HourlyEmployee>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int Count() => contents.Count;
+
+        public void Fill() => contents = EmployeeDA.GetProducts();
     }
 }
