da6ec2ee8485de0ba296191d4cc5a2dcae175fe8
Christian <ramoschristian39@gmail.com>
2023-04-18 21:15:45 -0400

    Completed the Hourly Employee Constants class

--- HourlyEmployeeMaintenanceApp/Domain/HourlyEmployeeConsts.cs
+++ HourlyEmployeeMaintenanceApp/Domain/HourlyEmployeeConsts.cs
@@ -7,10 +7,20 @@
  */
 namespace HourlyEmployeeMaintenanceApp.Domain
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    internal class HourlyEmployeeConsts
+
+    public static class HourlyEmployeeConsts
     {
+        /// <summary>Regular expression pattern for the id of the employee. </summary>
+        public const string IdPattern = @"HW-\d{4}";
+        /// <summary>Regular expression pattern for the name of the employee. </summary>
+        public const string NamePattern = @"[A-Z]{1}[a-z]{3,15}";
+        /// <summary>Minimum pay rate for the employee. </summary>
+        public const decimal MinPayRate = 8.50m;
+        /// <summary>Maximimum pay rate for the employee. </summary>
+        public const decimal MaxPayRate = 50.0m;
+        /// <summary>Minimum hours worked for the employee. </summary>
+        public const int MinHoursWorked = 0;
+        /// <summary>Maximum hours worked for the employee. </summary>
+        public const int MaxHoursWorked = 60;
     }
 }
