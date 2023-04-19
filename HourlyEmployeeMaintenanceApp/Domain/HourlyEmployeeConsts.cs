ae8cde86684b43136ea0c36ac146eba9ab32beff
Christian <ramoschristian39@gmail.com>
2023-04-18 21:18:53 -0400

    Date changed on comments

--- HourlyEmployeeMaintenanceApp/Domain/HourlyEmployeeConsts.cs
+++ HourlyEmployeeMaintenanceApp/Domain/HourlyEmployeeConsts.cs
@@ -2,7 +2,7 @@
  * File: HourlyEmployeeConsts.cs
  * Author: Christian Ramos Ortega 841184582
  * Course: COTI 4150-KJ1 Prof. Antonio F. Huertas
- * Date: 04/15/2023
+ * Date: 04/18/2023
  * Purpose: This class contains the constants to be used throughout the different classes.
  */
 namespace HourlyEmployeeMaintenanceApp.Domain
