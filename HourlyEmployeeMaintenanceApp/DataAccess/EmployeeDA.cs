12462f09287b29b82bf84cb26f74a10e88c2d1a7
Christian <ramoschristian39@gmail.com>
2023-04-18 23:12:12 -0400

    Syntax error fixed on EmployeeDA class.

--- HourlyEmployeeMaintenanceApp/DataAccess/EmployeeDA.cs
+++ HourlyEmployeeMaintenanceApp/DataAccess/EmployeeDA.cs
@@ -20,13 +20,13 @@ namespace HourlyEmployeeMaintenanceApp.DataAccess
         /// <returns>the list of products.</returns>
         public static List<HourlyEmployee> GetEmployees()
         {
-            var employees = new List<HourlyEmployee>();
+            var employees = new List<HourlyEmployee>()
             {
                 //Remember to validate SSecurityNum
                 new HourlyEmployee("HW-0011", "123456789", "Juan", CompanyDepartment.Sales, DateTime.Now.Date, 8.50m, 40),
                 new HourlyEmployee("HW-0022", "987654321", "Pedro", CompanyDepartment.Finance, DateTime.Now.Date, 9.50m, 48),
-                new HourlyEmployee("HW-0033", "111222333", "Luis", CompanyDepartment.InformationTechnology, DateTime.Now.Date, 11.50m, 25);
-            }
+                new HourlyEmployee("HW-0033", "111222333", "Luis", CompanyDepartment.InformationTechnology, DateTime.Now.Date, 11.50m, 25)
+            };
             return employees;
         }
     }
